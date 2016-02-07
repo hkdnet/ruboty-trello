8c41500c940e6af9e02c2cb578e3175f25df59ef
hkdnet <hkdnet@users.noreply.github.com>
2016-02-07 21:22:20 +0900

    extract method

--- lib/ruboty/trello/actions/add_card.rb
+++ lib/ruboty/trello/actions/add_card.rb
@@ -3,37 +3,48 @@ require 'trello'
 module Ruboty
   module Trello
     module Actions
+      # To add a card to trello
       class AddCard < ::Ruboty::Actions::Base
         def call
-          me = ::Trello::Member.find('me')
+          return message.reply "Board '#{message[:board_name]}' not found" if board.nil?
+          return message.reply "List '#{message[:list_name]}' not found" if list.nil?
 
-          board = me.boards.find { |board| board.name == message[:board_name] }
-          if board.nil?
-            message.reply "Board '#{message[:board_name]}' not found"
-            return
-          end
-
-          list = board.lists.find { |list| list.name == message[:list_name] }
-          if list.nil?
-            message.reply "List '#{message[:list_name]}' not found"
-            return
-          end
-
-          member_id = nil
-          if ENV['TRELLO_AUTO_ASSIGN'] && message.from_name
-            sender = message.from_name.downcase
-            member = board.members.find do |member|
-              member.username.downcase == sender || member.full_name.downcase.include?(sender)
-            end
-            member_id = member.try(:id)
-          end
-
-          new_card = ::Trello::Card.create(name: message[:name], list_id: list.id, member_ids: member_id)
-          if new_card.short_url
-            prefix = ENV['TRELLO_RESPONSE_PREFIX'] || 'Created'
-            message.reply "#{prefix} #{new_card.short_url}"
-          end
+          add_card
           false
+        end
+
+        def member
+          return nil unless ENV['TRELLO_AUTO_ASSIGN'] && message.from_name
+          @member ||= begin
+            board.members.find do |e|
+              e.username.downcase == sender || e.full_name.downcase.include?(sender)
+            end
+          end
+        end
+
+        def sender
+          message.from_name && message.from_name.downcase
+        end
+
+        def me
+          @me ||= ::Trello::Member.find('me')
+        end
+
+        def board
+          @board ||= me.boards.find { |e| e.name == message[:board_name] }
+        end
+
+        def list
+          @list ||= board.lists.find { |e| e.name == message[:list_name] }
+        end
+
+        def add_card
+          new_card = ::Trello::Card.create(name: message[:name],
+                                           list_id: list.id,
+                                           member_ids: member.try(:id))
+          return nil unless new_card.short_url
+          prefix = ENV['TRELLO_RESPONSE_PREFIX'] || 'Created'
+          message.reply "#{prefix} #{new_card.short_url}"
         end
       end
     end
