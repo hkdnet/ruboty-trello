d559ca1330afcc86edc34ed69a164dd0bc7ad98e
hkdnet <hkdnet@users.noreply.github.com>
2016-02-07 20:53:00 +0900

    list only one user's task and list only assigned to him/her

--- lib/ruboty/trello.rb
+++ lib/ruboty/trello.rb
@@ -11,8 +11,17 @@ end
 module Ruboty
   module Handlers
     class Trello < Base
-      on /trello\s+b\s+(?<board_name>.*)\s+l\s+(?<list_name>.*)\s+c\s+(?<name>.*)\z/i, name: 'add_card', description: 'Add card to Trello'
-      on /trello\s+list\z/i, name: 'list', description: 'List all tasks assigned to you'
+      LIST_PATTERN = /trello\s+list((\s+(?<assigned_only>assigned_only))|(\s+user:(?<user_name>\S+)))*/
+      on(
+        /trello\s+b\s+(?<board_name>.*)\s+l\s+(?<list_name>.*)\s+c\s+(?<name>.*)\z/i,
+        name: 'add_card',
+        description: 'Add card to Trello'
+      )
+      on(
+        LIST_PATTERN,
+        name: 'list',
+        description: 'List all tasks assigned to you'
+      )
 
       def add_card(message)
         Ruboty::Trello::Actions::AddCard.new(message).call
--- lib/ruboty/trello/actions/list.rb
+++ lib/ruboty/trello/actions/list.rb
@@ -5,18 +5,23 @@ module Ruboty
     module Actions
       class List < ::Ruboty::Actions::Base
         def call
+          user_name = message[:user_name] || 'me'
+          user = ::Trello::Member.find(user_name)
           msg = []
-          me = ::Trello::Member.find('me')
-
-          me.boards.each do |board|
-            msg.push "■ #{board.name}"
+          if user.nil?
+            return message.reply("User '#{user_name}' not found")
+          else
+            msg.push("#{user_name}'s task...")
+          end
+          user.boards.each do |board|
+            msg.push "  ■ #{board.name}"
             board.lists.each do |list|
               list.cards.each do |card|
-                msg.push "  [#{list.name}] #{card.name}"
+                next if message[:assigned_only] && !card.member_ids.include?(user.id)
+                msg.push "    [#{list.name}] #{card.name}"
               end
             end
           end
-
           message.reply(msg.join("\n"))
         end
       end
